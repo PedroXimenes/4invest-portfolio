a25d68217b90a131597e3eb0bb66cc8af29556bb
Pedro Ximenes <pedro.ximenes@hatchworks.com>
2023-10-07 10:57:53 -0300

    refatora pasta handlers

--- internal/pkg/api/router/router.go
+++ internal/pkg/api/router/router.go
@@ -1,11 +1,11 @@
 package router
 
 import (
-	"github.com/PedroXimenes/4invest/internal/app/handlers"
+	portfolioHandler "github.com/PedroXimenes/4invest/internal/app/handlers/portfolio"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Router(app *fiber.App) {
-	app.Get("/carteiras", handlers.GetAll)
-	app.Post("/carteiras", handlers.Create)
+	app.Get("/carteiras", portfolioHandler.GetAll)
+	app.Post("/carteiras", portfolioHandler.Create)
 }
