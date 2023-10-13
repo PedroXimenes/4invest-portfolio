5b3523035ad3bd85a6c7e412f2ecfaaf6cf70848
Pedro Ximenes <pedro.ximenes@hatchworks.com>
2023-10-13 16:40:21 -0300

    remove comentários e prints

--- internal/pkg/db/connection.go
+++ internal/pkg/db/connection.go
@@ -1,34 +1,5 @@
 package db
 
-// import (
-// 	"database/sql"
-// 	"fmt"
-
-// 	"github.com/PedroXimenes/4invest/configs"
-
-// 	_ "github.com/lib/pq"
-// )
-
-// func OpenConnection() (*sql.DB, error) {
-// 	conf := configs.GetDB()
-
-// 	sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s",
-// 		conf.Host,
-// 		conf.Port,
-// 		conf.User,
-// 		conf.Pass,
-// 		conf.Database)
-
-// 	conn, err := sql.Open("postgres", sc)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	err = conn.Ping()
-
-// 	return conn, err
-// }
-
 import (
 	"database/sql"
 
