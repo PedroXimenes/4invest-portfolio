0ee724bf279699e2c4d2c39c48589eea4057041f
Pedro Ximenes <pedro.ximenes@hatchworks.com>
2023-10-14 12:56:01 -0300

    adiciona url do front no cors

--- internal/pkg/api/router/router.go
+++ internal/pkg/api/router/router.go
@@ -8,12 +8,14 @@ import (
 )
 
 func Router(app *fiber.App) {
-	app.Use(cors.New())
-
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000, https://four-invest-front-p3xh7jp6wa-uc.a.run.app",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 	app.Get("/carteiras", portfolioHandler.GetAll)
-	app.Get("/carteiras/users/:id", portfolioHandler.Get)
+	app.Get("/carteiras/usuarios/:id", portfolioHandler.Get)
 	//app.Get("/carteiras/users/:id/", portfolioHandler.Get)
 	app.Post("/carteiras", portfolioHandler.Create)
-	app.Post("/assets", assetsHandler.Create)
-	app.Get("/assets/carteiras/:id", assetsHandler.Get)
+	app.Post("/ativos", assetsHandler.Create)
+	app.Get("/ativos/carteiras/:id", assetsHandler.Get)
 }
